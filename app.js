50b74524631526857d699c240a3000e1de83a35b
mike <micheal@visionmasterdesigns.com>
2011-04-20 00:34:06 +0530

    static to staticProvider as per latest connect

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-   app.use(express.static(__dirname + '/public'));
+   app.use(express.staticProvider(__dirname + '/public'));
    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
   var oneYear = 31557600000;
-  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(express.staticProvider(__dirname + '/public', { maxAge: oneYear }));
   app.use(express.errorHandler());
 });
 // Index Route
